47336a7f41fd8b2eec45aee8ebd3ce21399162f4
下舘朗 <shimodateakira@i.softbank.jp>
2020-11-26 16:24:20 +0900

    .NET5対応版を追加

---
+++ TestPrime2/Prime.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace TestPrime2
+{
+    /// <summary>
+    /// 素数を求めるクラス
+    /// </summary>
+    public class Prime : IEnumerable<int>
+    {
+        List<int> primes = new List<int>() { 2 };
+
+        private int primeCount = 0;
+
+        public int PrimeCount
+        {
+            get { return primeCount; }
+            set { primeCount = value; }
+        }
+
+        public Prime() { }
+
+        public Prime(int primeCount)
+        {
+            this.primeCount = primeCount;
+        }
+
+        public Task<List<int>> GetPrimesAsync()
+        {
+            return GetPrimesAsync(primeCount);
+        }
+
+        public void Reset()
+        {
+            primes = new List<int>() { 2 };
+            primeCount = 0;
+        }
+
+        /// <summary>
+        /// 引数 n までの素数を求める(同期型)
+        /// 結局、このメソッドは使わない。
+        /// </summary>
+        /// <param name="n">求める素数の最大数</param>
+        /// <returns>GetPrimesを実行するTask</returns>
+        public Task<List<int>> GetPrimesAsync(int n)
+        {
+            // 引数を渡して復帰値を得るTaskを生成
+            //return new Task<List<int>>(o => GetPrimes((int)o), n);
+
+            // 上記よりシンプルな記述
+            // 引数はnをキャプチャし使用している
+            return new Task<List<int>>(() => GetPrimes(n));
+        }
+
+        public List<int> GetPrimes()
+        {
+            return GetPrimes(primeCount);
+        }
+
+        /// <summary>
+        /// 引数 n までの素数を求める(非同期型)
+        /// </summary>
+        /// <param name="n">求める素数の最大数</param>
+        /// <returns>素数の配列</returns>
+        public List<int> GetPrimes(int n)
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                int primesCount = primes.Count;
+                bool notPrimeFlag = false;
+                for (int j = 0; j < primesCount; j++)
+                {
+                    if ((i % primes[j]) == 0)
+                    {
+                        notPrimeFlag = true;
+                        j = primesCount;
+                        break;
+                    }
+                }
+                if (!notPrimeFlag)
+                {
+                    primes.Add(i);
+                }
+            }
+            return primes;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            if(primes.Count > 1)
+            {
+                foreach (var item in primes)
+                {
+                    yield return item;
+                }
+                yield break;
+            }
+
+            for (int i = 2; i <= primeCount; i++)
+            {
+                int primesCount = primes.Count;
+                bool notPrimeFlag = false;
+                for (int j = 0; j < primesCount; j++)
+                {
+                    if ((i % primes[j]) == 0)
+                    {
+                        notPrimeFlag = true;
+                        j = primesCount;
+                        break;
+                    }
+                }
+                if (!notPrimeFlag)
+                {
+                    primes.Add(i);
+                    yield return i;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ TestPrime2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace TestPrime2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int PrimesCount = 1000000;
+            List<int> primes;
+            Prime prime = new Prime(PrimesCount);
+            Stopwatch sw = new Stopwatch();
+
+            // 同期で呼び出す
+            sw.Reset();
+            sw.Start();
+            primes = prime.GetPrimes();
+            sw.Stop();
+            Console.WriteLine("同期で呼び出す");
+            Console.WriteLine($"{nameof(sw.Elapsed)} = {sw.Elapsed}");
+            Console.WriteLine($"{nameof(primes.Count)} = {primes.Count}");
+#if DEBUG
+            Debug.WriteLine(string.Join(" ", primes.GetRange(0, 20)));
+#endif
+
+            // 同期で呼び出す
+            sw.Reset();
+            sw.Start();
+
+            // 非同期はこの呼び方でもよいが．．．．
+            Task<List<int>> task = prime.GetPrimesAsync();
+            task.Start();
+
+            // 利用する側(Programクラス)が非同期で呼び出す方が望ましい
+            //Task<List<int>> task = Task.Run(() => prime.GetPrimes(PrimesCount));
+
+            sw.Stop();
+
+            // タスクが終了するまで待つ。が．．．
+            // task.ResultがWaitを兼ねている。
+            // つまり、Resultを取得できるようになるまで待機する。
+            //task.Wait();
+
+            primes = task.Result;
+            Console.WriteLine("\n非同期で呼び出す");
+            Console.WriteLine($"{nameof(sw.Elapsed)} = {sw.Elapsed}");
+            Console.WriteLine($"{nameof(primes.Count)} = {primes.Count}");
+
+            // 列挙で呼び出す
+            //Console.WriteLine("");
+            //prime.Reset();
+            //prime.PrimeCount = PrimesCount;
+            //foreach (var item in prime)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+            //Console.WriteLine("\n\nPush any key!");
+            //Console.ReadKey();
+        }
+    }
+}
